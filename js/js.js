6431427e0e40203743e3fd47f450cfa0dee0bca2
Kurilova Yuliia <kurilova.yuliya8@gmail.com>
2019-11-15 23:45:49 +0200

    folders...

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $("#owl-example").owlCarousel({
+  	items: 2,
+  	navigation: true,
+  	navigationText: ["Предыдущий слайд", "Следующий слайд"],
+  	autoPlay: true
+  	})  	
+});
+
+  
